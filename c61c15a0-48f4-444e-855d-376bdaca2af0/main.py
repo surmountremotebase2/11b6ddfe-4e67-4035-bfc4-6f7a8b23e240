ab67905318451098f339deb77e71bd12c8ac24bb
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-30 19:18:34 -0400

    User Simon Gonzalez saved changes to main.py at 06/30/2024, 23:18:33 UTC time.

--- c61c15a0-48f4-444e-855d-376bdaca2af0/main.py
+++ c61c15a0-48f4-444e-855d-376bdaca2af0/main.py
@@ -1,5 +1,5 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import SMA, High
+from surmount.technical_indicators import SMA
 from surmount.logging import log
 
 class TradingStrategy(Strategy):

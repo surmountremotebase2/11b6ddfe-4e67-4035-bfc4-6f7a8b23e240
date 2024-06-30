482170cf0abd4c85e52db0a66d021703d1b1962c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-30 19:20:00 -0400

    User Simon Gonzalez saved changes to AI_Strategy_2.py at 06/30/2024, 23:19:59 UTC time.

---
+++ c61c15a0-48f4-444e-855d-376bdaca2af0/AI_Strategy_2.py
@@ -0,0 +1,49 @@
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA, BB
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        # Choose assets for trading
+        self.tickers = ["SPY", "QQQ"]
+        
+    @property
+    def interval(self):
+        # Set the data fetch interval to daily
+        return "1day"
+    
+    @property
+    def assets(self):
+        # List of assets this strategy trades
+        return self.tickers
+
+    @property
+    def data(self):
+        # Data required by the strategy
+        return []
+
+    def run(self, data):
+        allocation_dict = {}
+        for ticker in self.tickers:
+            # Calculate the Bollinger Bands for each asset
+            bollinger_bands = BB(ticker, data["ohlcv"], 20)
+            if bollinger_bands is not None:
+                current_price = data["ohlcv"][-1][ticker]['close']
+                # Check if the current price is touching the upper band (resistance)
+                if current_price >= bollinger_bands['upper'][-1]:
+                    # If touching resistance, prepare to short sell, here we just sell
+                    allocation_dict[ticker] = 0  # 0 allocation represents a sell signal
+                elif current_price <= bollinger_bands['lower'][-1]:
+                    # If touching lower band (support), buy or increase position
+                    allocation_dict[ticker] = 1 / len(self.tickers)  # Equally divided allocation among tickers
+                else:
+                    # If within bands, do nothing
+                    allocation_dict[ticker] = allocation_dict.get(ticker, 0)
+            else:
+                # If Bollinger Bands can't be calculated, do nothing
+                allocation_dict[ticker] = allocation_dict.get(ticker, 0)
+        
+        # Log the allocation decision
+        log("Allocations: " + str(allocation_dict))
+        
+        return TargetAllocation(allocation_dict)

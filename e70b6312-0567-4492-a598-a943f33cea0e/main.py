4b966c36a4c4a5314ae1aec9794037b1072bf748
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-01 14:37:02 -0400

    User Simon Gonzalez saved changes to main.py at 07/01/2024, 18:37:01 UTC time.

--- e70b6312-0567-4492-a598-a943f33cea0e/main.py
+++ e70b6312-0567-4492-a598-a943f33cea0e/main.py
@@ -28,6 +28,6 @@ class TradingStrategy(Strategy):
             if tuple(i)[0]=="insider_trading":
                 if data[tuple(i)] and len(data[tuple(i)])>0:
                 if "Sale" in data[tuple(i)][-1]['transactionType']:
-                    allocation_dict[tuple(i)[1]] = 0
+                allocation_dict[tuple(i)[1]] = 0
 
         return TargetAllocation(allocation_dict)

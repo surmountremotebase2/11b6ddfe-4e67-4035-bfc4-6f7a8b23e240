088e85011470403b039080362951f090648a556c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-01 17:01:29 -0400

    User Simon Gonzalez saved changes to main.py at 07/01/2024, 21:01:28 UTC time.

--- 7b3a58d1-ce7c-4eca-9f11-905adfde311b/main.py
+++ 7b3a58d1-ce7c-4eca-9f11-905adfde311b/main.py
@@ -6,7 +6,7 @@ import numpy as np
 class TradingStrategy(Strategy):
     def __init__(self):
         # Define pair of tickers for statistical arbitrage
-        
+        self.tickers = ["GC Z 17"]
         
         # Set the lookback period for mean and standard deviation calculation
         self.lookback_short = 20  # Shorter time frame

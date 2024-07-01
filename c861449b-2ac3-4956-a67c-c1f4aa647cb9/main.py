47c37e6152dc67112771a05ed7c3835e450ac98d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-01 17:44:14 -0400

    User Simon Gonzalez saved changes to main.py at 07/01/2024, 21:44:13 UTC time.

--- c861449b-2ac3-4956-a67c-c1f4aa647cb9/main.py
+++ c861449b-2ac3-4956-a67c-c1f4aa647cb9/main.py
@@ -1,42 +1,33 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.data import InsiderTrading
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
+from surmount.logging import log
+from surmount.data import Asset, InstitutionalOwnership, InsiderTrading
 
 class TradingStrategy(Strategy):
+
     def __init__(self):
-        # List of tickers to monitor for insider trading activities
-        self.tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META"]
-        # Create an InsiderTrading data object for each ticker
-        self.data_list = [InsiderTrading(ticker) for ticker in self.tickers]
+        self.tickers = ["SPY", "QQQ", "AAPL", "GOOGL"]
+        self.data_list = [InstitutionalOwnership(i) for i in self.tickers]
+        self.data_list += [InsiderTrading(i) for i in self.tickers]
 
     @property
     def interval(self):
-        # Setting the interval property to daily
         return "1day"
 
     @property
     def assets(self):
-        # Specifying the assets that the strategy will consider
         return self.tickers
-    
+
     @property
     def data(self):
-        # Returning the list of data objects (InsiderTrading)
         return self.data_list
 
     def run(self, data):
-        allocation_dict = {}
+        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
         for i in self.data_list:
-            # Checking for the most recent insider trade for each asset
-            recent_trade = data[tuple(i)].pop() if data[tuple(i)] else None
-            if recent_trade:
-                if recent_trade['transactionType'] == "Purchase":
-                    # If the latest insider trade is a purchase, allocate a portion of the portfolio to this stock
-                    allocation_dict[tuple(i)[1]] = 1.0 / len(self.tickers)
-                else:
-                    # If the latest insider trade is not a purchase (e.g., sale), do not allocate any to this stock
+            if tuple(i)[0]=="insider_trading":
+                if data[tuple(i)] and len(data[tuple(i)])>0:
+                if "Sale" in data[tuple(i)][-1]['transactionType']:
                     allocation_dict[tuple(i)[1]] = 0
-            else:
-                # If there is no recent trade data, set allocation to zero
-                allocation_dict[tuple(i)[1]] = 0
 
         return TargetAllocation(allocation_dict)

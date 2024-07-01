ce1bb513f2f4a5b31c9a8d31cd2b28195bbdcd43
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-01 16:30:46 -0400

    User Simon Gonzalez saved changes to main.py at 07/01/2024, 20:30:45 UTC time.

--- c861449b-2ac3-4956-a67c-c1f4aa647cb9/main.py
+++ c861449b-2ac3-4956-a67c-c1f4aa647cb9/main.py
@@ -23,12 +23,4 @@ class TradingStrategy(Strategy):
         return self.data_list
 
     def run(self, data):
-
-        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
-        for i in self.data_list:
-            if tuple(i)[0]=="insider_trading":
-                if data[tuple(i)] and len(data[tuple(i)])>0:
-                    if "Sale" in data[tuple(i)][-1]['transactionType']:
-                        allocation_dict[tuple(i)[1]] = 0
-
-        return TargetAllocation(allocation_dict)
+        print(data)
